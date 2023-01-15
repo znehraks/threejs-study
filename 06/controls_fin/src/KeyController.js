4b104e28834a4e159fccf26af753f99f548a64e4
jayArchisketch <jay@archisketch.com>
2023-01-15 21:13:41 +0900

    study: particle 까지 공부

---
+++ 06/controls_fin/src/KeyController.js
@@ -0,0 +1,17 @@
+export class KeyController {
+  constructor() {
+    // 생성자
+    this.keys = [];
+
+    window.addEventListener("keydown", (e) => {
+      console.log(this.keys);
+      console.log(e.code + " 누름");
+      this.keys[e.code] = true;
+    });
+
+    window.addEventListener("keyup", (e) => {
+      console.log(e.code + " 뗌");
+      delete this.keys[e.code];
+    });
+  }
+}
